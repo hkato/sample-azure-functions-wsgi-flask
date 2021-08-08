9e6fe089c2740657dc3dba06594aff2d2abf3b04
Hideyuki KATO <hideyuki@kato.jp>
2021-08-08 12:52:42 +0900

    Change from raw Functions to WsgiMiddleware to use Flask

--- foo/__init__.py
+++ foo/__init__.py
@@ -2,23 +2,11 @@ import logging
 
 import azure.functions as func
 
+from app import app
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    # WSGI Application
+    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
